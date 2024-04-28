968897c2ad87e75e5434baa23f9664fe8b68ff89
Salman7D <salmanrayhan753@gmail.com>
2024-04-28 11:28:59 +0600

    implement react-typewriter

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,11 @@
 import { Helmet } from "react-helmet";
 import HomeSpot from "../HomeSpot";
+import { Fade } from "react-awesome-reveal";
+import { Typewriter } from "react-simple-typewriter";
 
 
 const Home = () => {
+
 
     return (
         <div>
@@ -11,6 +14,35 @@ const Home = () => {
                 <title>Home - Skyline Residence</title>
                 <link rel="canonical" href="http://mysite.com/example" />
             </Helmet>
+            
+            <Fade direction="down">
+                <h1>Welcome to my world</h1>
+            </Fade>
+            
+            <h1 style={{margin: "50px"}}>
+                Tourism is {" "}
+                <span style={{fontWeight: "bold,", color: "green"}}
+                >
+                <Typewriter
+                words={['Awesome', 'Cool', 'Peace', 'Fantasy']}
+                loop={5}
+                cursor
+                cursorStyle='_'
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+                
+                
+                >
+
+
+                </Typewriter>
+                
+                
+                </span>
+
+                
+                </h1>
 
             <HomeSpot></HomeSpot>
             
--- src/Components/MyList.jsx
+++ src/Components/MyList.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import SpotCard from "./SpotCard";
 import { useState } from "react";
+import { Fade } from "react-awesome-reveal";
 
 
 const MyList = () => {
@@ -12,9 +13,12 @@ const MyList = () => {
         <div className="m-20">
             <h2 className="text-6xl text-center my-20 text-purple-600 mb-10">Total Spots: {spots.length}</h2>
             <div className="grid md:grid-cols-2 gap-4">
-            {
+                <Fade direction="right">
+                {
                 spots.map(spot => <SpotCard key={spot._id} spot={spot} spots={spots} setSpots={setSpots}></SpotCard>)
-            }
+                }
+                </Fade>
+           
             </div>
         </div>
     );
